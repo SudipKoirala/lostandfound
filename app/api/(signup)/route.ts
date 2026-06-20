d6b3e15f973d717aba68f0bfa0a42cf9c1409a9d
Sudip Koirala <soodeep321@gmail.com>
2026-06-20 23:58:03 +0545

    commit

---
+++ app/api/(signup)/route.ts
@@ -0,0 +1,28 @@
+import { User } from "@/model/user";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest)=>{
+    const {firstName, lastName, userName, password, confirmPassword} = await req.json()
+    if(!firstName || !lastName || !userName || !password || !confirmPassword){
+        return NextResponse.json({
+            message: "Fill all the fields"
+        }, {status: 400})
+    }
+
+    if(password !== confirmPassword){
+        return NextResponse.json({
+            message: "Password and Confirm pass"
+        }, {status: 400})
+    }
+
+
+    const user = await User.create({
+        firstName, lastName, userName, password
+    })
+
+    return NextResponse.json({
+        message: "USer created", user
+    }, {
+        status: 200
+    })
+}
